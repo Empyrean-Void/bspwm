87b66d2cb23d75028d8eb83fe27d15c143abc1ac
Empyrean-Void <jshortz@proton.me>
2023-03-31 14:13:06 -0600

    Added foundation for different themes

---
+++ themes/polybar.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+## Files and Directories
+STYLE="voyage"
+
+launch_bar() {
+  # External monitor (Nvidia)
+  if [[ $(xrandr -q | grep "HDMI-0 connected") ]];
+     then
+	   bash "$HOME"/.config/bspwm/themes/"$STYLE"/polybar/launch-nvidia.sh
+  fi
+
+  # Internel monitor (Nvidia)
+  if [[ $(xrandr -q | grep "HDMI-0 disconnected") ]];
+     then
+	   bash "$HOME"/.config/bspwm/themes/"$STYLE"/polybar/launch-nvidia.sh
+  fi
+
+  # Internal monitor (Intel)
+  if [[ $(xrandr -q | grep "eDP1") ]];
+    then
+	   bash "$HOME"/.config/bspwm/themes/"$STYLE"/polybar/launch-intel.sh
+  fi
+}
+
+launch_bar
