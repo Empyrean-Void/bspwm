90daa43eb2d856c66e70839a76a9a588febd2a54
Empyrean-Void <jshortz@proton.me>
2023-03-31 15:17:49 -0600

    Fixed bug in polybar launch

--- themes/polybar.sh
+++ themes/polybar.sh
@@ -7,19 +7,19 @@ launch_bar() {
   # External monitor (Nvidia)
   if [[ $(xrandr -q | grep "HDMI-0 connected") ]];
      then
-	   bash "$HOME"/.config/bspwm/themes/"$STYLE"/polybar/launch-nvidia.sh
+	   bash ~/.config/bspwm/themes/"$STYLE"/polybar/launch-nvidia.sh
   fi
 
   # Internel monitor (Nvidia)
   if [[ $(xrandr -q | grep "HDMI-0 disconnected") ]];
      then
-	   bash "$HOME"/.config/bspwm/themes/"$STYLE"/polybar/launch-nvidia.sh
+	   bash ~/.config/bspwm/themes/"$STYLE"/polybar/launch-nvidia.sh
   fi
 
   # Internal monitor (Intel)
   if [[ $(xrandr -q | grep "eDP1") ]];
     then
-	   bash "$HOME"/.config/bspwm/themes/"$STYLE"/polybar/launch-intel.sh
+	   bash ~/.config/bspwm/themes/"$STYLE"/polybar/launch-intel.sh
   fi
 }
 
