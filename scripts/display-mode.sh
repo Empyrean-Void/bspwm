11a2f30afee5d660c955e08a552ab61fabf0350a
Empyrean-Void <jshortz@proton.me>
2023-04-04 21:43:02 -0600

    Added check to prevent turning off laptop display when un-docked

--- scripts/display-mode.sh
+++ scripts/display-mode.sh
@@ -9,12 +9,19 @@ RASI="$DIR/themes/$STYLE/rofi/config.rasi"
 chosen=$(printf "󰍺  Dual Display\n󰍹  External\n󰍹  Laptop" | rofi -dmenu -config ${RASI} -i -p "Display Mode:")
 
 case "$chosen" in
-	"󰍺  Dual Display") xrandr --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal \
-             --output eDP-1-1 --mode 1920x1080 --pos 0x1080 --rotate normal;;
-	"󰍹  External") xrandr --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal \
-             --output eDP-1-1 --off;;
-	"󰍹  Laptop") xrandr --output eDP-1-1 --mode 1920x1080 --pos 0x0 --rotate normal \
-             --output HDMI-0 --off;;
+	"󰍺  Dual Display")
+        xrandr --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal \
+            --output eDP-1-1 --mode 1920x1080 --pos 0x1080 --rotate normal;;
+	"󰍹  External") 
+        if [[ $(xrandr -q | grep "HDMI-0 disconnected") ]];
+        then
+            exit 1
+        fi
+        xrandr --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal \
+            --output eDP-1-1 --off;;
+	"󰍹  Laptop")
+        xrandr --output eDP-1-1 --mode 1920x1080 --pos 0x0 --rotate normal \
+            --output HDMI-0 --off;;
 	*) exit 1 ;;
 esac
 
