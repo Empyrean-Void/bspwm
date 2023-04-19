7cd5a3a2885fd0eccd09cfb65a1b48d26dd8f385
Empyrean-Void <jshortz@proton.me>
2023-04-18 18:35:38 -0600

    Added Rofi calendar (WIP)

---
+++ scripts/rofi-cal.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env sh
+
+highlight="#d04c4c"
+
+month() {
+        m=${1:-this}
+        cal --monday $(date --date="$m month" +"%m %Y")\
+                | sed "/^\\s*$/d"
+}
+
+highlight_today() {
+        d=$(date +%e)
+        sed "s/^$d[^0-9]/<span color='$highlight'>$d<\/span> /g" \
+                | sed "s/ $d / <span color='$highlight'>$d<\/span> /g" \
+                | sed "s/ $d$/ <span color='$highlight'>$d<\/span>/g"
+}
+
+calendar() {
+        if [[ "$1" == "long" ]]; then
+                month last
+                echo
+        fi
+        month | highlight_today
+        if [[ "$1" == "long" ]]; then
+                echo
+                month next
+        fi
+}
+
+rofi -e "$(calendar $1)" \
+        -config ~/.config/bspwm/themes/samurai/rofi/cal.rasi \
+        -markup
