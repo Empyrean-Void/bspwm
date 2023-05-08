e17d954230f30000b1ca4180b55630e05c60e291
Empyrean-Void <jshortz@proton.me>
2023-05-08 16:22:57 -0600

    Moved display layout to external script, moved exports to external script, reorganized scripts folder

--- scripts/display-layout.sh
+++ scripts/display-layout.sh
@@ -5,7 +5,6 @@ if [[ $(xrandr -q | grep "HDMI-0 connected") ]];
              --output eDP-1-1 --mode 1920x1080 --pos 0x1080 --rotate normal 
       bspc monitor HDMI-0 -d 1 2 3 4 5
       bspc monitor eDP-1-1 -d 6 7 8 9 10
-      exec $BSPDIR/themes/polybar.sh &
 fi
 
 # Internel monitor (Nvidia)
@@ -14,12 +13,10 @@ if [[ $(xrandr -q | grep "HDMI-0 disconnected") ]];
       xrandr --output eDP-1-1 --mode 1920x1080 --pos 0x0 --rotate normal \
              --output HDMI-0 --off 
       bspc monitor eDP-1-1 -d 1 2 3 4 5
-      exec $BSPDIR/themes/polybar.sh &
 fi
 
 # Internal monitor (Intel)
 if [[ $(xrandr -q | grep "eDP1") ]];
   then
     bspc monitor eDP1 -d 1 2 3 4 5
-    exec $BSPDIR/themes/polybar.sh &
 fi
