f5ce6198d3444afc379708052d50df9f41dcc685
Empyrean-Void <jshortz@proton.me>
2023-05-11 12:04:19 -0600

    Updated notification center (still WIP)

--- scripts/notify-log.sh
+++ scripts/notify-log.sh
@@ -2,9 +2,41 @@
 
 logfile=$1
 
-dbus-monitor "interface='org.freedesktop.Notifications'" |\
-grep --line-buffered "string" |\
-grep --line-buffered -e method -e ":" -e '""' -e urgency -e notify -v |\
-grep --line-buffered '.*(?=string)|(?<=string).*' -oPi |\
-xargs -I '{}' \
-printf "---$( date )---\n---"{}"\n" >> $logfile
+declare -a MSGBUF
+STATE=off
+MSGTIME=
+
+printbuf() {
+  JOINED=$(printf "%s\n" "${MSGBUF[@]}")
+  printf "%s\n%s\n" "--- ${MSGTIME} ---" "${JOINED}"
+}
+
+procmsg() {
+  if [[ "${1}" =~ member=Notify$ ]]; then
+    STATE=on
+    MSGTIME=$(date '+%Y-%m-%d %H:%M:%S')
+    MSGBUF=()
+  elif [[ "${1}" =~ member=NotificationClosed$ ]]; then
+    STATE=off
+    printbuf >> "$logfile"
+  else
+    if [[ "${STATE}" == "on" ]]; then
+      if [[ "${1}" =~ ^string ]]; then
+        case "${1}" in
+          "string \"\"") ;;
+          "string \"urgency\"") ;;
+          "string \"sender-pid\"") ;;
+          *)
+            STR=$(echo "${1}" | sed 's/^string "\(.*\)"$/\1/')
+            MSGBUF+=("${STR}")
+            ;;
+        esac
+      fi
+    fi
+  fi
+}
+
+dbus-monitor "interface='org.freedesktop.Notifications'" |
+  while read -r line; do
+    procmsg "$line"
+  done
--- scripts/notify-view.sh
+++ scripts/notify-view.sh
@@ -5,5 +5,14 @@
 DIR="$HOME/.config/bspwm"
 STYLE="gruvbox"
 RASI="$DIR/themes/$STYLE/rofi/notify.rasi"
+LOG=~/.log/dunst-log
 
-cat ~/.log/dunst-log | awk -F'---' '{print $2 "\n" $3}' | sed 's/^[[:space:]]*//' | sed '/^$/d' | rofi -dmenu -config ${RASI} -i -p "Notifications: "
+get_date() {
+    awk -F'---' '{print $2}' $LOG | sed 's/^[[:space:]]*//' | sed '/^$/d' | uniq 
+}
+
+get_content() {
+    awk -F'string "' '{gsub(/,$/,""); print $3, $4}' $LOG | sed '/^%/{N;d;}' | tr -d '\n' | sed 's/[,"]//g'
+}
+
+(get_date && get_content) | rofi -dmenu -config ${RASI} -i -p "Notifications: "
